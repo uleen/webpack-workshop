f5f0311c1085ab26315e329d00c0871d832b6275
Aleksandr Nikulin <uleen@chelaputka.com>
2019-12-30 11:44:19 +0300

    2 step

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,3 +1,22 @@
 const webpack = require("webpack");
+const merge = require("webpack-merge");
+const path = require("path");
 
-module.exports = mode => {};
+const {
+  basePreset,
+  developmentPreset,
+  productionPreset
+} = require("./Presets");
+
+const PATHS = {
+  app: path.join(__dirname, "../src"),
+  dist: path.join(__dirname, "../dist"),
+  index: path.join(__dirname, "../index")
+};
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(basePreset(), productionPreset(), { mode });
+  }
+  return merge(basePreset(), developmentPreset(), { mode });
+};
